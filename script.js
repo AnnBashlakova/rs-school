2d1578e18bd1a292bd1f8279b37669194fcf918c
Anna <anna47297@gmail.com>
2021-09-19 17:37:51 +0300

    docs:add style

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
-window.addEventListener('DOMContentLoaded', () => {
-    const menu = document.querySelector('.menu'),
-    menuItem = document.querySelectorAll('.menu_item'),
+window.addEventListener('DOMContentLoaded', ()  => {
+    const menu = document.querySelector('.list'),
+    menuItem = document.querySelectorAll('.item'),
     hamburger = document.querySelector('.hamburger');
 
     hamburger.addEventListener('click', () => {
         hamburger.classList.toggle('hamburger_active');
-        menu.classList.toggle('menu_active');
+        menu.classList.toggle('list_active');
     });
 
     menuItem.forEach(item => {
         item.addEventListener('click', () => {
             hamburger.classList.toggle('hamburger_active');
-            menu.classList.toggle('menu_active');
+            menu.classList.toggle('list_active');
         })
     })
 })
